759c2c7ceb7a0c4332b6b54f58b5e473aeb123b8
Amine <BoyWonder@Amines-MacBook-Pro.local>
2013-05-06 16:02:37 +0200

    files modified

--- src/HalloAmine.java
+++ src/HalloAmine.java
@@ -5,5 +5,9 @@ public class HalloAmine {
 	public static void main(String[] args) {
 		System.out.println("Hallo Amine, guck aufn Boden, du hast dein Ohr verlohren!XD");
 	}
+	
+	public void mimiistbloed(){
+		//du bist ja so blšd
+	}
 
 }
